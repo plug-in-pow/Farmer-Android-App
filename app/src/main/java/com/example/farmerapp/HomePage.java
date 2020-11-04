f4372ca3c8206c6b5e4a4349bd9f8dbe95f03ed0
plug-in-pow <tripathimayank36@gmail.com>
2020-11-04 22:41:18 +0530

    Added theme

--- app/src/main/java/com/example/farmerapp/HomePage.java
+++ app/src/main/java/com/example/farmerapp/HomePage.java
@@ -1,9 +1,11 @@
 package com.example.farmerapp;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class HomePage extends AppCompatActivity {
 
@@ -12,7 +14,15 @@ public class HomePage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.topAppBar);
         setSupportActionBar(toolbar);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
+
 }
